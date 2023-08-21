80af66dba031c092cdb5b65c1df979c7f37e42d1
Wenping-Cui <wenpingcui@gmail.com>
2023-08-21 15:12:57 -0700

    add bash script for runing codes

---
+++ run_parallel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+
+SGE_TASK_ID=1
+
+rR_array=()
+for i in $(seq -6. 0.2 -1.5); do 
+    output=$(bc -l <<<"e($i*l(10))")
+    rR_array+=($output)
+done
+
+rR_array=( $(shuf -e ${rR_array[@]}) )
+
+Ninput=(1000000 10000000)
+
+Linput=$(seq 30 10 50)
+
+Jinput=(0.005)
+
+
+
+parallel --jobs 28 main.jl  --T 250000 --taskID $SGE_TASK_ID --threadID {%} --d 2 --J {1} --N {2} --rR_hosthost {3} --rH_phagehost {3} --L {4} --save_dir "results/diff_genetransf_noNconstraint/"  --population_constraint "no" --output_details true ::: ${Jinput[@]}   ::: ${Ninput[@]} ::: ${rR_array[@]} ::: ${Linput[@]} 
